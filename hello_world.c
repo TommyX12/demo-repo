9bd77a6400895a18fde9e29c5db7ca5c056a442d
TommyX12 <tommyx058@gmail.com>
2017-10-03 20:01:07 -0400

    Added helper method

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+
+
+
+void help_function()
+{
+    printf("Hi\n");
+}
+
 int main(int argc, char *argv[]) {
     printf("Hello World!\n");
+    help_function();
     return 0;
 }
